fb713149e381a8837a3b266f01d7be0a71f34a26
smita-09 <smita.aquarius@gmail.com>
2021-06-24 18:52:45 +0200

    Class creation

---
+++ OOPs/Set1.py
@@ -0,0 +1,7 @@
+# Creating a Test class which says hello
+class Test:
+    def fun(self):
+        print("hello")
+# Instantiating an object
+obj = Test()
+obj.fun() # Calling the function of the class Test
